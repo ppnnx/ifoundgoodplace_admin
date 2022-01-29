d9f3a437bd10d56fd495fcbcebcd857eb731a5bd
pnn-97 <partlyclouddy@gmail.com>
2022-01-29 20:40:33 +0700

    rewrite code

--- lib/widgets/user/user_default_widget.dart
+++ lib/widgets/user/user_default_widget.dart
@@ -14,98 +14,102 @@ class _UserDefaultWidgetState extends State<UserDefaultWidget> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: APIUser.getUser(),
-        builder: (context, AsyncSnapshot<List<AllUsers>> snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-                shrinkWrap: true,
-                physics: BouncingScrollPhysics(),
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  AllUsers user = snapshot.data![index];
+      future: APIUser.getUser(),
+      builder: (context, AsyncSnapshot<List<AllUsers>> snapshot) {
+        if (snapshot.hasData) {
+          return ListView.builder(
+            shrinkWrap: true,
+            physics: BouncingScrollPhysics(),
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              AllUsers user = snapshot.data![index];
 
-                  return Container(
-                    padding: EdgeInsets.only(left: 16.0, right: 16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          child: ListTile(
-                            onTap: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => ProfileScreen(
-                                            id: user.iduser,
-                                          )));
-                            },
-                            leading: checkstatus(user),
-                            title: Container(
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    user.username!,
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                ],
+              return Container(
+                padding: EdgeInsets.only(left: 16.0, right: 16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: ListTile(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ProfileScreen(
+                                id: user.iduser,
                               ),
                             ),
-                            subtitle: Text(
-                              user.email!,
-                              style: TextStyle(
-                                fontSize: 13,
+                          );
+                        },
+                        leading: checkstatus(user),
+                        title: Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                user.username!,
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                ),
                               ),
-                            ),
-                            trailing: user.image == null || user.image == " "
-                                ? CircleAvatar(
-                                    radius: 30,
-                                    backgroundImage:
-                                        AssetImage('assets/second.png'),
-                                  )
-                                : ClipRRect(
-                                    borderRadius:
-                                        BorderRadius.circular(10000.0),
-                                    child: CachedNetworkImage(
-                                      imageUrl:
-                                          'http://35.213.159.134/avatar/${user.image}',
-                                      width: 60,
-                                      height: 60,
-                                      fit: BoxFit.cover,
-                                      placeholder: (context, url) {
-                                        return Center(
-                                          child: CircularProgressIndicator(),
-                                        );
-                                      },
-                                      errorWidget: (context, url, error) {
-                                        return CircleAvatar(
-                                          radius: 30,
-                                          backgroundColor: Colors.black12,
-                                          child: Icon(
-                                            Icons.error,
-                                            color: Colors.red,
-                                          ),
-                                        );
-                                      },
-                                    ),
-                                  ),
+                            ],
                           ),
                         ),
-                        Divider(color: Colors.black),
-                      ],
+                        subtitle: Text(
+                          user.email!,
+                          style: TextStyle(
+                            fontSize: 13,
+                          ),
+                        ),
+                        trailing: user.image == null || user.image == " "
+                            ? CircleAvatar(
+                                radius: 30,
+                                backgroundImage: AssetImage(
+                                  'assets/second.png',
+                                ),
+                              )
+                            : ClipRRect(
+                                borderRadius: BorderRadius.circular(10000.0),
+                                child: CachedNetworkImage(
+                                  imageUrl:
+                                      'http://35.213.159.134/avatar/${user.image}',
+                                  width: 60,
+                                  height: 60,
+                                  fit: BoxFit.cover,
+                                  placeholder: (context, url) {
+                                    return Center(
+                                      child: CircularProgressIndicator(),
+                                    );
+                                  },
+                                  errorWidget: (context, url, error) {
+                                    return CircleAvatar(
+                                      radius: 30,
+                                      backgroundColor: Colors.black12,
+                                      child: Icon(
+                                        Icons.error,
+                                        color: Colors.red,
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                      ),
                     ),
-                  );
-                });
-          }
-
-          return Center(
-            child: CircularProgressIndicator(),
+                    Divider(color: Colors.black),
+                  ],
+                ),
+              );
+            },
           );
-        });
+        }
+
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
   }
 
   checkstatus(AllUsers user) {
